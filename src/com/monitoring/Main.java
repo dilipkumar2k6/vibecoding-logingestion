0a1e45f7f0b3964eb04dc3e8f34726f74f4f0131
Dilip <dilip.kumar2k6@gmail.com>
2026-01-02 06:12:11 -0800

    Create Main.java

---
+++ src/com/monitoring/Main.java
@@ -0,0 +1,15 @@
+package com.monitoring;
+
+public class Main {
+    public static void main(String[] args) {
+        AlertService alertService = message -> System.out.println("ALERT: " + message);
+        ServerHealth health = new ServerHealth(alertService);
+
+        // Simulate some logs
+        health.ingest("2023-10-27 10:00:00 INFO System started");
+        health.ingest("2023-10-27 10:00:01 ERROR Connection failed");
+        health.ingest("2023-10-27 10:00:02 INFO Retrying...");
+        
+        System.out.println("Current Error Rate: " + health.getErrorRate());
+    }
+}
